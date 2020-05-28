848c1bb2640335ae155f7288ebbf6c2fa5b1b0d9
Henrique <suporte@pcmsolucoes.com.br>
2020-05-28 20:22:39 -0300

    Fim da aula 3.

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -7,8 +7,17 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$student = new Student(null, 'Guilherme Henrique', new \DateTimeImmutable('1995-10-15'));
+$student = new Student(
+    null,
+    'Aluno teste',
+    new \DateTimeImmutable('1994-05-15')
+);
 
-$sqlInsert = "INSERT INTO students (name, birth_date) values ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}')";
+$sqlInsert = "INSERT INTO students (name, birth_date) values (:name, :birth_date)";
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue(':name', $student->name());
+$statement->bindValue(':birth_date', $student->birthDate()->format('Y-m-d'));
 
-var_dump($pdo->exec($sqlInsert));
+if ($statement->execute()) {
+    echo "Aluno incluído";
+}
